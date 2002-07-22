39324deb16b58f64d026fe17d2664770018b835b
sijtsche <sijtsche>
2002-07-22 14:19:06 +0000

    new elements and classes added and order of constants changed because of representation errors in output

--- org/w3c/css/parser/CssSelectorsConstant.java
+++ org/w3c/css/parser/CssSelectorsConstant.java
@@ -1,5 +1,5 @@
 //
-// $Id: CssSelectorsConstant.java,v 1.2 2002-04-08 21:24:12 plehegar Exp $
+// $Id: CssSelectorsConstant.java,v 1.3 2002-07-22 14:19:06 sijtsche Exp $
 // From Philippe Le Hegaret (Philippe.Le_Hegaret@sophia.inria.fr)
 // Updated September 20th 2000 Sijtsche de Jong (sy.de.jong@let.rug.nl)
 //
@@ -7,62 +7,69 @@
 // Please first read the full copyright statement in file COPYRIGHT.html
 /*
  * $Log: CssSelectorsConstant.java,v $
- * Revision 1.2  2002-04-08 21:24:12  plehegar
- * New
+ * Revision 1.3  2002-07-22 14:19:06  sijtsche
+ * new elements and classes added and order of constants changed because of representation errors in output
+ *
+ * Revision 1.2  2002/05/23 09:03:47  dejong
+ * new elements and classes added and order of constants changed due to representation errors in output
  *
  */
 package org.w3c.css.parser;
 
 /**
- * @version $Revision: 1.2 $
+ * @version $Revision: 1.3 $
  */
 public interface CssSelectorsConstant {
 
     public static final String[] PSEUDOCLASS_CONSTANTS =
-    { "first-child", 
-      "link", "visited", 
-      "target", 
-      "hover", "active", "focus", "enabled", "disabled",
-      "checked", "indeterminate", "root", "last-child", 
+    { "link", "visited", "target", "active",
+      "hover", "focus", "first-child",
+      "enabled", "disabled",
+      "checked", "indeterminate", "root", "last-child",
       "first-of-type", "last-of-type", "only-of-type",
       "only-child", "empty" };
+
+    public static final String[] PSEUDOCLASS_CONSTANTSCSS2 =
+    {
+      "link", "visited", "target", "active",
+      "hover", "focus", "first-child"
+    };
+
+    public static final String[] PSEUDOCLASS_CONSTANTSCSS1 =
+    {
+      "link", "visited", "target", "active"
+    };
+
+	public static final String[] PSEUDOCLASS_CONSTANTS_MOBILE =
+	{
+	   "link", "visited", "active", "focus"
+	};
 
     // lang is special (and not available for mobile profile)
 
     public static final String[] PSEUDOELEMENT_CONSTANTS =
-    { "first-line", "first-letter", "before", "after", 
-      "selection", "menu", "subject"
-    };
-
-    public static final String[] PSEUDOCLASS_CONSTANTSCSS2 =
-    { "first-child", 
-      "link", "visited", 
-      "target", 
-      "hover", "active", "focus"
-    };
-
-    public static final String[] PSEUDOCLASS_CONSTANTSCSS1 =
-    { 
-      "link", "visited", 
-      "target", "active"
+    { "first-line", "first-letter", "before", "after",
+      "selection"
     };
 
     // lang is special, and contains and nth-element and nth-type-of
 
     public static final String[] PSEUDOELEMENT_CONSTANTSCSS2 =
     { "first-line", "first-letter",
-      "before", "after"  
+      "before", "after"
     };
 
-    public static final String[] PSEUDOELEMENT_CONSTANTSCSS1 = 
+    public static final String[] PSEUDOELEMENT_CONSTANTSCSS1 =
     {
-	"first-line", "first-letter"
+		"first-line", "first-letter"
     };
 
     /** [lang="fr"] */
     public static final int ATTRIBUTE_EXACT = ' ';
     /** [lang~="fr"] */
     public static final int ATTRIBUTE_ONE_OF = '~';
+    /** class selector == like [lang~="fr"] */
+    public static final int ATTRIBUTE_CLASS_SEL = '.';
     /** [lang|="fr"] */
     public static final int ATTRIBUTE_BEGIN = '|';
     /** [lang] */
@@ -77,10 +84,10 @@ public interface CssSelectorsConstant {
     /** Maximun of ATTRIBUTE_ONE_OF */
     public static final int ATTRIBUTE_LENGTH = 10;
 
-    /** descendant connector  */    
+    /** descendant connector  */
     public static final char DESCENDANT = ' ';
     /** child connector */
     public static final char CHILD = '>';
     /** adjacent connector */
     public static final char ADJACENT = '+';
-}  
+}
