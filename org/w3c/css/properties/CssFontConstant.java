1af69015f6fb4eb833f7c2aa314ee56bf328a5cb
sijtsche <sijtsche>
2002-12-18 09:21:01 +0000

    values changed

--- org/w3c/css/properties/CssFontConstant.java
+++ org/w3c/css/properties/CssFontConstant.java
@@ -1,12 +1,15 @@
 //
-// $Id: CssFontConstant.java,v 1.2 2002-04-08 21:17:43 plehegar Exp $
+// $Id: CssFontConstant.java,v 1.3 2002-12-18 09:21:01 sijtsche Exp $
 // From Philippe Le Hegaret (Philippe.Le_Hegaret@sophia.inria.fr)
 //
 // (c) COPYRIGHT MIT and INRIA, 1997.
 // Please first read the full copyright statement in file COPYRIGHT.html
 /*
  * $Log: CssFontConstant.java,v $
- * Revision 1.2  2002-04-08 21:17:43  plehegar
+ * Revision 1.3  2002-12-18 09:21:01  sijtsche
+ * values changed
+ *
+ * Revision 1.2  2002/04/08 21:17:43  plehegar
  * New
  *
  * Revision 1.3  1997/07/30 13:19:59  plehegar
@@ -22,48 +25,50 @@
 package org.w3c.css.properties;
 
 /**
- * @version $Revision: 1.2 $
+ * @version $Revision: 1.3 $
  */
 public interface CssFontConstant {
 
   /**
    * Array of font-style values
-   */  
+   */
   static String[] FONTSTYLE = { "normal", "italic", "oblique", "inherit" };
 
   /**
    * Array of font-variant values
-   */  
+   */
   static String[] FONTVARIANT = { "normal", "small-caps", "inherit" };
 
   /**
    * Array of font-size values
-   */  
-  static String[] FONTSIZE = { 
+   */
+  static String[] FONTSIZE = {
     "xx-small", "x-small", "small", "medium", "large", "x-large", "xx-large",
     "larger", "smaller", "inherit"
   }; // relative-size
 
   /**
    * Array of font-weight values
-   */  
-  static String[] FONTWEIGHT = { "normal", "bold", "bolder", 
+   */
+  static String[] FONTWEIGHT = { "normal", "bold", "bolder",
 				 "lighter", "inherit" };
-  
+
   /**
    * Array of font-stretch values
-   */  
-  static String[] FONTSTRETCH = { "normal", "wider", "narrower", 
-				  "ultra-condensed", "extra-condensed", 
-				  "condensed", "semi-condensed", 
+   */
+  static String[] FONTSTRETCH = { "normal", "wider", "narrower",
+				  "ultra-condensed", "extra-condensed",
+				  "condensed", "semi-condensed",
 				  "semi-expanded", "expanded", "extra-expanded",
 				  "ultra-expanded", "inherit" };
-  
+
   /**
    * Array of font values
-   */  
-  static String[] FONT = { "caption", "icon", "menu", 
-			   "message-box", "small-caption", 
-			   "status-bar", "inherit" };
+   */
+  static String[] FONT = { "icon", "window", "status-bar", "document", "workspace", "desktop",
+  						   "tooltip", "message-box", "dialog", "small-caption", "caption", "button",
+  						   "default-button", "hyperlink", "menu", "pull-down-menu", "pop-up-menu",
+  						   "list-menu", "field", "inherit"
+	  					 };
 
 }
