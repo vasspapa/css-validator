e230a97365fbd5a2669ff0b4b39920db4c47aa9f
julien <julien>
2007-08-13 08:30:36 +0000

    Using translation to display the css version

--- org/w3c/css/parser/CssPropertyFactory.java
+++ org/w3c/css/parser/CssPropertyFactory.java
@@ -1,5 +1,5 @@
 //
-// $Id: CssPropertyFactory.java,v 1.19 2007-08-07 10:53:39 julien Exp $
+// $Id: CssPropertyFactory.java,v 1.20 2007-08-13 08:30:36 julien Exp $
 // From Philippe Le Hegaret (Philippe.Le_Hegaret@sophia.inria.fr)
 //
 // (c) COPYRIGHT MIT and INRIA, 1997.
@@ -21,7 +21,7 @@ import org.w3c.css.values.CssExpression;
 import org.w3c.css.values.CssIdent;
 
 /**
- * @version $Revision: 1.19 $
+ * @version $Revision: 1.20 $
  * @author Philippe Le H�garet
  */
 public class CssPropertyFactory implements Cloneable {
@@ -200,7 +200,7 @@ public class CssPropertyFactory implements Cloneable {
 		// this is an error... or a warning if it exists in another
 		// profile... FIXME
 		if (classname == null) {
-			throw new InvalidParamException("noexistence", property, ac.getFormatedCssVersion(), ac);
+			throw new InvalidParamException("noexistence", property, ac.getMsg().getString(ac.getCssVersion()), ac);
 		}
 
 		CssIdent initial = new CssIdent("initial");
