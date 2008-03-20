46d669b292762fc7e7fef4429f10f5f2089c4b92
ylafon <ylafon>
2008-03-20 16:51:17 +0000

    now cache hashcode, to avoid too much unnecessary computing

--- org/w3c/css/values/CssIdent.java
+++ org/w3c/css/values/CssIdent.java
@@ -1,5 +1,5 @@
 //
-// $Id: CssIdent.java,v 1.5 2005-09-14 15:15:33 ylafon Exp $
+// $Id: CssIdent.java,v 1.6 2008-03-20 16:51:17 ylafon Exp $
 // From Philippe Le Hegaret (Philippe.Le_Hegaret@sophia.inria.fr)
 //
 // (c) COPYRIGHT MIT and INRIA, 1997.
@@ -9,64 +9,70 @@ package org.w3c.css.values;
 import org.w3c.css.util.ApplContext;
 
 /**
- * @version $Revision: 1.5 $
+ * @version $Revision: 1.6 $
  */
 public class CssIdent extends CssValue {
 
-  /**
-   * Create a new CssIdent
-   */
-  public CssIdent() {
-  }
+    private int hashcode = 0;
 
-  /**
-   * Create a new CssIdent
-   *
-   * @param s The identificator
-   */
-  public CssIdent(String s) {
-    value = s;
-  }
+    /**
+     * Create a new CssIdent
+     */
+    public CssIdent() {
+    }
+    
+    /**
+     * Create a new CssIdent
+     *
+     * @param s The identificator
+     */
+    public CssIdent(String s) {
+	value = s;
+    }
 
-  /**
-   * Set the value of this ident.
-   *
-   * @param s     the string representation of the identificator.
-   * @param frame For errors and warnings reports.
-   */
-  public void set(String s, ApplContext ac) {
-    value = s;
-  }
+    /**
+     * Set the value of this ident.
+     *
+     * @param s     the string representation of the identificator.
+     * @param frame For errors and warnings reports.
+     */
+    public void set(String s, ApplContext ac) {
+	value = s;
+	hashcode = 0;
+    }
 
-  /**
-   * Returns the internal value.
-   */
-  public Object get() {
-    return value;
-  }
+    /**
+     * Returns the internal value.
+     */
+    public Object get() {
+	return value;
+    }
 
-  /**
-   * Returns a string representation of the object.
-   */
-  public String toString() {
-    return value;
-  }
+    /**
+     * Returns a string representation of the object.
+     */
+    public String toString() {
+	return value;
+    }
 
-  /**
-   * Compares two values for equality.
-   *
-   * @param value The other value.
-   */
-  public boolean equals(Object value) {
-    return (value instanceof CssIdent && value.hashCode() == hashCode());
-  }
+    /**
+     * Compares two values for equality.
+     *
+     * @param value The other value.
+     */
+    public boolean equals(Object value) {
+	return (value instanceof CssIdent && value.hashCode() == hashCode());
+    }
 
-  /**
-   * Returns a hashcode for this ident.
-   */
-  public int hashCode() {
-    return value.toLowerCase().hashCode();
-  }
+    /**
+     * Returns a hashcode for this ident.
+     */
+    public int hashCode() {
+	if (hashcode == 0) {
+	    hashcode = value.toLowerCase().hashCode();
+	}
+	return hashcode;
+    }
 
-  private String value;
+    private String value;
 }
