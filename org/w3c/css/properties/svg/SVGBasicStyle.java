0fcce812a0ee687e6cde0cbabfdc052ad522ae0a
ylafon <ylafon>
2008-04-07 14:32:41 +0000

    wrong class type before cast

--- org/w3c/css/properties/svg/SVGBasicStyle.java
+++ org/w3c/css/properties/svg/SVGBasicStyle.java
@@ -1,5 +1,5 @@
 //
-// $Id: SVGBasicStyle.java,v 1.2 2008-04-07 14:16:24 ylafon Exp $
+// $Id: SVGBasicStyle.java,v 1.3 2008-04-07 14:32:41 ylafon Exp $
 // From Sijtsche de Jong
 //
 // COPYRIGHT (c) 1995-2002 World Wide Web Consortium, (MIT, INRIA, Keio University)
@@ -160,7 +160,7 @@ public class SVGBasicStyle extends SVGTinyStyle {
 	if (mask == null) {
 	    mask =
 		(Mask) style.CascadingOrder (
-		    new ImageRendering(), style, selector);
+		    new Mask(), style, selector);
 	}
 	return mask;
     }
