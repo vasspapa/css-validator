07874406ba06888332b0129a9acf3ff15c9bb46a
ylafon <ylafon>
2009-02-06 13:18:50 +0000

    pre-alpha json output

--- org/w3c/css/servlet/CssValidator.java
+++ org/w3c/css/servlet/CssValidator.java
@@ -1,5 +1,5 @@
 //
-// $Id: CssValidator.java,v 1.37 2007-09-13 10:15:33 julien Exp $
+// $Id: CssValidator.java,v 1.38 2009-02-06 13:18:50 ylafon Exp $
 // From Philippe Le Hegaret (Philippe.Le_Hegaret@sophia.inria.fr)
 //
 // (c) COPYRIGHT MIT and INRIA, 1997.
@@ -47,7 +47,7 @@ import org.w3c.www.mime.MimeTypeFormatException;
 /**
  * This class is a servlet to use the validator.
  * 
- * @version $Revision: 1.37 $
+ * @version $Revision: 1.38 $
  */
 public final class CssValidator extends HttpServlet {
 
@@ -63,7 +63,9 @@ public final class CssValidator extends HttpServlet {
 
     final static String soap12      = "application/soap+xml";
 
-    final static String server_name = "Jigsaw/2.2.2 "
+    final static String json      = "application/json";
+
+    final static String server_name = "Jigsaw/2.2.5 "
 	+ "W3C_CSS_Validator_JFouffa/2.0";
 
     final static String headers_name = "X-W3C-Validator-";
@@ -699,6 +701,8 @@ public final class CssValidator extends HttpServlet {
 	    output = "html";
 	} else if (soap12.equals(output)) {
 	    output = "soap12";
+	} else if (json.equals(output)) {
+	    output = "json";
 	} else if(textplain.equals(output)) {
 	    output = "text";
 	}
@@ -760,6 +764,13 @@ public final class CssValidator extends HttpServlet {
 	    //outputMt = MimeType.TEXT_PLAIN.getClone();
 	} else if(output.equals("ucn")) {
 		outputMt = MimeType.APPLICATION_XML.getClone();
+        } else if(output.equals("json")) {
+            try {
+                outputMt = new MimeType(json);
+            }
+            catch (MimeTypeFormatException e) {
+                outputMt = MimeType.TEXT_PLAIN.getClone();
+            }
 	} else {
 	    // Change this line if you want text/html output when incorrect
 	    // output is passed
