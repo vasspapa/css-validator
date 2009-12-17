b04e189dbf5cdef6537c55dc64ad3a8707044616
ylafon <ylafon>
2009-12-17 16:09:44 +0000

    ident caching of some values + static accessors

--- org/w3c/css/values/CssIdent.java
+++ org/w3c/css/values/CssIdent.java
@@ -1,17 +1,40 @@
 //
-// $Id: CssIdent.java,v 1.7 2008-03-25 18:30:11 ylafon Exp $
+// $Id: CssIdent.java,v 1.8 2009-12-17 16:09:44 ylafon Exp $
 // From Philippe Le Hegaret (Philippe.Le_Hegaret@sophia.inria.fr)
 //
 // (c) COPYRIGHT MIT and INRIA, 1997.
 // Please first read the full copyright statement in file COPYRIGHT.html
 package org.w3c.css.values;
 
+import java.util.HashMap;
+
 import org.w3c.css.util.ApplContext;
 
 /**
- * @version $Revision: 1.7 $
+ * @version $Revision: 1.8 $
  */
 public class CssIdent extends CssValue {
+
+    public static HashMap<String,CssIdent> allowedvalues;
+    static {
+	allowedvalues = new HashMap<String,CssIdent>();
+	allowedvalues.put("inherit", new CssIdent("inherit"));
+	allowedvalues.put("none", new CssIdent("inherit"));
+    }
+    /**
+     * Get a cached CssIdent, useful for common values like "inherit"
+     * @param name, the ident name
+     * @return a CssIdent
+     */
+    public static CssIdent getIdent(String name) {
+	CssIdent val = allowedvalues.get(name);
+	if (val != null) {
+	    return val;
+	}
+	val = new CssIdent(name);
+	allowedvalues.put(name, val);
+	return val;
+    }
 
     public static final int type = CssTypes.CSS_IDENT;
     
